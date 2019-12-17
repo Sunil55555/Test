7f5bce0801473a8192c3e9b5626d872e2517fd47
Sunil55555 <58981079+Sunil55555@users.noreply.github.com>
2019-12-17 19:03:57 +0530

    Add files via upload

---
+++ ConsoleAppGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test code");
+            Console.ReadLine();
+        }
+    }
+}
