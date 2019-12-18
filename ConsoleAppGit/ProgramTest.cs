4f653e2c5c84f1a0e5cb89b9716fa467bb8a26c4
Sunil55555 <58981079+Sunil55555@users.noreply.github.com>
2019-12-18 16:56:55 +0530

    Add files via upload

---
+++ ConsoleAppGit/ProgramTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleAppGit
+{
+    [TestFixture]
+    class ProgramTest
+    {
+        [Test]
+        public void test()
+        {
+            Program p = new Program();
+            Assert.AreEqual(56, p.calculate());
+        }
+    }
+}
